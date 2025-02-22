a504d02228f4ef5529d1ea17215f90f71f9ba143
Noname <example-hexlet@example.com>
2025-02-23 01:44:40 +0500

    Исправление 2го пунта по заданию

--- gendiff.js
+++ gendiff.js
@@ -5,8 +5,13 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .helpOption('-h, --help', 'output usage information');
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(`Comparing files: ${filepath1} and ${filepath2}`);
+  });
 
-program.parse();
+program.parse(process.argv);
