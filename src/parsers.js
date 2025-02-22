e6a83c888a6ab30c0444689d624f4a4c137261f2
Noname <example-hexlet@example.com>
2025-02-23 02:05:02 +0500

    Созданы 2 json файла и добавлена папка src

---
+++ src/parsers.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import path from 'node:path';
+
+const parseFile = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  const fileContent = fs.readFileSync(absolutePath, 'utf-8');
+  return JSON.parse(fileContent);
+};
+
+export default parseFile;
