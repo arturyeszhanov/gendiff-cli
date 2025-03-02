b142f8386d02ce53dc01b8e528813783416b8992
Noname <example-hexlet@example.com>
2025-03-02 17:51:34 +0500

    Правки 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-#!/usr/bin/env node
-
+import path from 'path';
+import { readFileSync } from 'fs';
 import parseFile from './parsers.js';
 import buildDiff from './buildDiff.js';
 import format from './formatters/index.js';
 
+const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
+
+const readFile = (filepath) => {
+  const fullPath = getFullPath(filepath);
+  const data = readFileSync(fullPath, 'utf-8');
+  const format = path.extname(filepath).slice(1); // 'json', 'yaml', etc.
+  return parseFile(data, format);
+};
+
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const data1 = parseFile(filepath1);
-  const data2 = parseFile(filepath2);
+  const data1 = readFile(filepath1);
+  const data2 = readFile(filepath2);
+
   const diff = buildDiff(data1, data2);
 
   return format(diff, formatName);
