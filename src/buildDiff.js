852ceec900329abd7f47957b89d5cac5ef478448
Artur Yeszhanov <arturyeszhanov@gmail.com>
2025-03-03 16:24:02 +0500

    fix buildDiff 6

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -15,8 +15,8 @@ const buildDiff = (data1, data2) => {
     }
     if (!_.isEqual(data1[key], data2[key])) {
       return {
-        key, type: 'changed', oldValue: data1[key], newValue: data2[key]
-    };
+        key, type: 'changed', oldValue: data1[key], newValue: data2[key],
+      };
     }
     return { key, type: 'unchanged', value: data1[key] };
   });
